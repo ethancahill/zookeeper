2bd2bf9002463a757a92f2feca38883668fdf964
Ethan Cahill <andersmith29@gmail.comhtml lang=en  head    meta charset=UTF-8 /  /head  body    p      I LOVE PANDASgit config --global user.email andersmith29@gmail.com>
2022-05-07 12:18:45 -0400

    server.js update

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express')
-
+const { animals } = require('./data/animals')
 const app = express();
+
+app.get('/api/animals', (req, res) => {
+    res.send('Hello!');
+});
 
 app.listen(3001, () => {
     console.log('API server now on port 3001!');
